23838e81529719e296e5ffcf7444ab5d991fd89e
SolanaG <gomez.solanarocio@gmail.com>
2023-07-10 15:12:04 -0300

    delete comments

--- star-wars-api-microservices/characters/src/data/index.js
+++ star-wars-api-microservices/characters/src/data/index.js
@@ -13,15 +13,3 @@ module.exports = {
     return await axios.post("http://database:8004/Character", body);
   },
 };
-
-// const characters = require("./character.json");
-
-// module.exports = {
-//   list: async () => {
-//     return characters;
-//   },
-
-//   create: async () => {
-//     throw Error("Hubo un error al crear el personaje");
-//   },
-// };
